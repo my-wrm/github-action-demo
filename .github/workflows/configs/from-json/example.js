0cff76ac7b4a999850cb0aa67da4a16732c058aa
Thada Wangthammang <thada.wth@gmail.com>
2022-06-20 10:07:09 +0700

    demo json

--- .github/workflows/configs/from-json/example.js
+++ .github/workflows/configs/from-json/example.js
@@ -2,10 +2,10 @@
 
 /** @type import('./type').IConfig */
 const config = {
-  web: [
-    { name: 'server-1', ip: '123.456.78.90' },
-    { name: 'server-2', ip: '123.456.78.111' },
-  ],
+  // web: [
+  //   { name: 'server-1', ip: '123.456.78.90' },
+  //   { name: 'server-2', ip: '123.456.78.111' },
+  // ],
   db: [{ dbserver: 'server-3', ip: '123.456.78.90' }],
 };
 
